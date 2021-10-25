7c5dcdff0e26c8b0a566720d37cae8809d330548
Vasya Golovastov <VSGolovastov@gmail.com>
2021-10-25 11:03:09 +0400

    Version_25.10.21

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "World";
+        System.out.println(hello + " " + world + " !");
+        /*int a = 1;
+        int b = 2;
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));*/
+        /*int c = a +b;
+        System.out.println("sum = " + c);*/
+       /* System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        *//*System.out.println("2..");
+        System.out.println("3...");
+        System.out.println("Hello World!");*/
+    }
+}

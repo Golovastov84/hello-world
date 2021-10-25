1dab00e4f9f79eb1ba0e732236777b71a5e7655b
Golovastov84 <87019319+Golovastov84@users.noreply.github.com>
2021-10-25 11:11:20 +0400

    ! > a

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         String hello = "Hello";
         String world = "World";
-        System.out.println(hello + " " + world + " !");
+        System.out.println(hello + " " + world + " a");
         /*int a = 1;
         int b = 2;
         System.out.println("a > b = " + (a > b));

a472bfcdfe406598b0cbc352c4bf1a3dec15b0c4
Golovastov84 <87019319+Golovastov84@users.noreply.github.com>
2021-10-25 12:13:50 +0400

    2_a>!

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         String hello = "Hello";
         String world = "World";
-        System.out.println(hello + " " + world + " a");
+        System.out.println(hello + " " + world + " !");
         }
 }
